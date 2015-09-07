1c71dc4bf61c3252369d00b87683e384df7d6e25
Luan Pham <thanhluan001@gmail.com>
2015-09-07 13:59:44 +0200

    Add new test file

---
+++ src/Testfile.py
@@ -0,0 +1,12 @@
+"""Hello there. This is a new file import.
+
+"""
+import numpy as np
+
+
+    print("Hello World")
+
+    combTemp = new Did()
+    t = t + combTemp
+    f = t * 2 * combTemp + 4
+    combTemp = f
