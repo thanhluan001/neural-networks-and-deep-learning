f33cbd19dbfed0461fa4036b5752d95649e979f7
Luan Pham <thanhluan001@gmail.com>
2015-09-07 23:26:04 +0200

    Add new file and delete the test file

---
+++ src/run.py
@@ -0,0 +1,9 @@
+""" A script to run the exercise
+"""
+import mnist_loader
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data)
